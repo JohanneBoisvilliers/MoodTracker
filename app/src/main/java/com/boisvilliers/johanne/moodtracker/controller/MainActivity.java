d52b056ac3d69d331d09603ca0687e7e86fa78ec
Johanne <johanne.boisvilliers@gmail.com>
2018-05-01 11:54:51 +0200

    add ressources to project (png and colors.xml)

--- app/src/main/java/com/boisvilliers/johanne/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/boisvilliers/johanne/moodtracker/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.boisvilliers.johanne.moodtracker;
+package com.boisvilliers.johanne.moodtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.boisvilliers.johanne.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
