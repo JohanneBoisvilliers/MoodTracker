61bfc6ad091a1f212cff5f9e311ffab18267e571
Johanne <johanne.boisvilliers@gmail.com>
2018-05-01 14:32:50 +0200

    I changed png's names (replaced - by _) and add : dimen.xml files for adaptive images sizes

--- app/src/main/java/com/boisvilliers/johanne/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/boisvilliers/johanne/moodtracker/controller/MainActivity.java
@@ -1,15 +1,25 @@
 package com.boisvilliers.johanne.moodtracker.controller;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.boisvilliers.johanne.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout mLinearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
