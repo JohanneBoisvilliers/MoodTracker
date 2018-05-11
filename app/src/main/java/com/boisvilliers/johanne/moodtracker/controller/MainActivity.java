8722561951f27d03950d9b58551195df6b551b08
Johanne <johanne.boisvilliers@gmail.com>
2018-05-11 14:55:46 +0200

    add shared preferences to keep the last mood in memory. If user change(or not) mood and quit the app, when he'll restart the app the last mood is the current mood, all of this with gson/json and SharedPreferences

--- app/src/main/java/com/boisvilliers/johanne/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/boisvilliers/johanne/moodtracker/controller/MainActivity.java
@@ -3,10 +3,12 @@ package com.boisvilliers.johanne.moodtracker.controller;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,6 +18,11 @@ import com.boisvilliers.johanne.moodtracker.R;
 import com.boisvilliers.johanne.moodtracker.model.ConstructList;
 import com.boisvilliers.johanne.moodtracker.view.EditTextAddComments;
 import com.boisvilliers.johanne.moodtracker.view.LayoutConstructor;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,29 +30,36 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mSmiley;
     private ImageButton mAddComments,mHistory;
     private GestureDetectorCompat mDetector;
-    protected int currentView = 3;
+    protected int mCurrentView = 3;
     private Context mContext = this;
     private LayoutConstructor mMainHierarchy;
-    private int[] mListColor,mListSmiley;
+    protected int[] mListColor,mListSmiley;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    protected ArrayList<Integer> mThingsToSave;
+    private ArrayList<Integer> mThingsToLoad;
+    public static final String KEY_VIEW = "KEY_VIEW";
+    Gson gson;
+    String json;
 
-    /*OnTouchEvent get the gestureDirection in GestureDetectorListener and then add or remove 1 to currentView
-     which contain the index of the current view in Hierarchy View's list.
-    After that, refreshing the view with setContentView.*/
+    /*OnTouchEvent get the gestureDirection in GestureDetectorListener and then add or remove 1 to mCurrentView
+     which contain the index of the current smiley and background color.
+    After that, refresh the view with refreshView method.*/
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         this.mDetector.onTouchEvent(event);
         char gestureDirection = GestureDetectorListener.getGestureDirection();
         if (gestureDirection =='U'){
-            if(currentView<4){
-                int viewToReduce = currentView;
-                currentView+=1;
-                refreshView(currentView);
+            if(mCurrentView <4){
+                int viewToReduce = mCurrentView;
+                mCurrentView +=1;
+                refreshView(mCurrentView);
             }
         }
         if (gestureDirection =='D'){
-            if(currentView>0){
-                currentView-=1;
-                refreshView(currentView);
+            if(mCurrentView >0){
+                mCurrentView -=1;
+                refreshView(mCurrentView);
             }
         }
         return super.onTouchEvent(event);
@@ -55,31 +69,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preferences = getPreferences(MODE_PRIVATE);
         mDetector = new GestureDetectorCompat(this,new GestureDetectorListener());
         mSmiley=new ImageView(this);
         mMainHierarchy=new LayoutConstructor(this);
-
+        mThingsToSave = new ArrayList<Integer>();
+        mThingsToLoad = new ArrayList<Integer>();
+        //initialize differents views
         mCurrentFrameLayout =findViewById(R.id.mainActivity_global);
         mCurrentFrameLayout =mMainHierarchy.getViewsHierarchy();
         mSmiley = mCurrentFrameLayout.findViewById(R.id.image_smiley);
         mAddComments = mCurrentFrameLayout.findViewById(R.id.Addcomments_button);
         mHistory = mCurrentFrameLayout.findViewById(R.id.History_button);
-
+        //get color's list and smiley's list
         mListColor=new ConstructList().getColorArray();
         mListSmiley=new ConstructList().getSmileyArray();
-
-        this.refreshView(currentView);
+        //try to get view saved in SharedPreferences
+        loadSharedPreferences();
+        Log.d("TAG","truc a save = " + json);
         addListenerOnAddCommentsButton();
         addListenerOnHistoryButton();
     }
-
+    //method who refresh the view thanks to mCurrentView ( mCurrentView is transforming to index of each list : mListColor and mListSmiley)
     public void refreshView(int index){
         mCurrentFrameLayout.setBackgroundColor(getResources().getColor(mListColor[index]));
         mSmiley.setImageResource(mListSmiley[index]);
         setContentView(mCurrentFrameLayout);
-
+        mThingsToSave.clear();
+        mThingsToSave.add(getResources().getColor(mListColor[mCurrentView]));
+        mThingsToSave.add(mListSmiley[mCurrentView]);
+        mThingsToSave.add(mCurrentView);
     }
-    //add a listener on buttons
+    //add a listener on buttons to know when user clicked on them and to know what to do
     public void addListenerOnHistoryButton() {
         //listener and actions for History button
         mHistory.setOnClickListener(new View.OnClickListener() {
@@ -115,5 +136,35 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
+    }
+    //save the color and the smiley which are corresponding to the day's mood
+    public void saveSharedPreferences(){
+        gson = new Gson();
+        json = gson.toJson(mThingsToSave);
+        editor = preferences.edit();
+        editor.putString(KEY_VIEW,json).apply();
+    }
+    //load the last color and smiley in SharedPreferences
+    public void loadSharedPreferences(){
+        gson = new Gson();
+        Type type = new TypeToken<ArrayList<Integer>>(){}.getType();
+        json=preferences.getString(KEY_VIEW,"");
+        if(json.isEmpty())
+            //Add the background color and the smiley by default if there is nothing in the shared preferences
+            refreshView(mCurrentView);
+        else {
+            mThingsToLoad = gson.fromJson(json, type);
+            mCurrentView= mThingsToLoad.get(2);
+            mCurrentFrameLayout.setBackgroundColor(getResources().getColor(mListColor[mCurrentView]));
+            mSmiley.setImageResource(mListSmiley[mCurrentView]);
+            setContentView(mCurrentFrameLayout);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        saveSharedPreferences();
+        Log.d("TAG","truc a save = " + json);
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/boisvilliers/johanne/moodtracker/model/ConstructList.java
+++ app/src/main/java/com/boisvilliers/johanne/moodtracker/model/ConstructList.java
@@ -9,36 +9,20 @@ public class ConstructList  {
 
     private int[] mSmileyArray;
     private int[] mColorArray;
-
+    /*Constructor which create 2 arrays, one for keeping all the smileys and the other all the colors. Both array are sorted for each element
+     is corresponding to the other element's array at the same index(mSmileyArray[1] is matching mColorArray[1]*/
     public ConstructList() {
         mSmileyArray =new int[]{R.drawable.smiley_sad,R.drawable.smiley_disappointed,R.drawable.smiley_normal,R.drawable.smiley_happy,R.drawable.smiley_super_happy};
         mColorArray =new int[]{R.color.faded_red,R.color.warm_grey,R.color.cornflower_blue_65,R.color.light_sage,R.color.banana_yellow};
     }
-
+    //getter for return smiley array
     public int[] getSmileyArray() {
         return mSmileyArray;
     }
-
+    //getter for return color array
     public int[] getColorArray() {
         return mColorArray;
     }
-
-    /*private Context mContext;
-    private ArrayList<LayoutConstructor> mListLayout=new ArrayList<>();
-
-        //Constructor who create the view's hierarchy as list
-        public ConstructList(Context current){
-            this.mContext=current;
-            mListLayout.add(new LayoutConstructor(mContext,mContext.getResources().getColor(R.color.faded_red),R.drawable.smiley_sad));
-            mListLayout.add(new LayoutConstructor(mContext,mContext.getResources().getColor(R.color.warm_grey),R.drawable.smiley_disappointed));
-            mListLayout.add(new LayoutConstructor(mContext,mContext.getResources().getColor(R.color.cornflower_blue_65),R.drawable.smiley_normal));
-            mListLayout.add(new LayoutConstructor(mContext,mContext.getResources().getColor(R.color.light_sage),R.drawable.smiley_happy));
-            mListLayout.add(new LayoutConstructor(mContext,mContext.getResources().getColor(R.color.banana_yellow),R.drawable.smiley_super_happy));
-        }
-
-        public ArrayList<LayoutConstructor> getListLayout() {
-            return mListLayout;
-        }*/
-    }
+}
 
 
